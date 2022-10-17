c364624bd675afa9fc2e909418bda0dd217a13d6
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2022-10-17 09:53:31 +0100

    feat(vta-890): es lint change interating to indexed for loop

--- src/eventbridge/send.ts
+++ src/eventbridge/send.ts
@@ -1,11 +1,11 @@
-// import { EventBridge } from 'aws-sdk';
+import { EventBridge } from 'aws-sdk';
 import { EventEntry } from './EventEntry';
 import { Entries } from './Entries';
 import { SendResponse } from './SendResponse';
 import logger from '../observability/logger';
 import { MCRequest } from '../utils/MCRequest';
 
-// const eventbridge = new EventBridge();
+const eventbridge = new EventBridge();
 // eslint-disable-next-line @typescript-eslint/require-await
 const sendMCProhibition = async (mcRequests: MCRequest[]): Promise<SendResponse> => {
   const sendResponse: SendResponse = {
@@ -13,7 +13,8 @@ const sendMCProhibition = async (mcRequests: MCRequest[]): Promise<SendResponse>
     FailCount: 0,
   };
   try {
-    mcRequests.forEach((x) => {
+    for (let i = 0; i < mcRequests.length; i++) {
+      const x = mcRequests[i];
       const entry: EventEntry = {
         Source: process.env.AWS_EVENT_BUS_SOURCE_MC,
         Detail: `{ "testResult": "${JSON.stringify(x).replace(/"/g, '\\"')}" }`,
@@ -27,14 +28,14 @@ const sendMCProhibition = async (mcRequests: MCRequest[]): Promise<SendResponse>
       params.Entries.push(entry);
 
       logger.debug(`event about to be sent: ${JSON.stringify(params)}`);
-      // TODO comment out when testing
-      // const result = await eventbridge.putEvents(params).promise();
-      // logger.info(
-      //   `${result.Entries.length} event sent to eventbridge.`,
-      // );
+      // eslint-disable-next-line no-await-in-loop
+      const result = await eventbridge.putEvents(params).promise();
+      logger.info(
+        `${result.Entries.length} event sent to eventbridge.`,
+      );
       console.log('event send to eventbridge');
       sendResponse.SuccessCount++;
-    });
+    }
   } catch (error) {
     logger.error('', error);
     sendResponse.FailCount++;
