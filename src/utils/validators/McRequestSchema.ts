3697c4fe418566d54235209adcc709bd394394af
Daniel Searle <84069850+Daniel-Searle@users.noreply.github.com>
2022-12-19 10:49:22 +0000

    VTA-1123 - additional test types (#3)
    * VTA-890 - Create new mc-prohibitions repo  (#1) (#2)
    
    * feat(vta-890): prohib code
    
    feat(vta-890): husky issue
    
    feat(vta-890): node engine warning
    
    feat(vta-890): dependency increase
    
    feat(vta-890): jest version downgrade
    
    feat(vta-890): commented out event bridge call
    
    feat(vta-890): eslint
    
    feat(vta-890): formatting
    
    feat(vta-890): gitignore
    
    feat(vta-890): remove semantic release
    
    feat(vta-890): change for loop to for each
    
    feat(vta-890): es lint change interating to indexed for loop
    
    feat(vta-890): added additional logs for put event
    
    feat(vta-890): rename event bus name env
    
    feat(vta-890): removed logs
    
    feat(vta-890): removed logs
    
    feat(vta-890): change to detail
    
    feat(vta-890): added tools-setup
    
    feat(vta-890): added test result id to request
    
    feat(vta-890): rename of map interface
    
    feat(vta-890): change to trail flag
    
    feat(vta-890): eslint
    
    feat(vta-890): commit with broken unit tests, push for sul
    
    feat(vta-890): fixed unit tests
    
    feat(vta-890): renamed variables to appropriate names
    
    feat(vta-890): downgrade joi version number for more readable response
    
    feat(vta-890): rename mock
    
    feat(vta-890): removed logger
    
    feat(vta-890): removed logger
    
    feat(vta-890): removed logger
    
    * feat(vta-890): added additional logger
    
    * feat(vta-890): added additional logger
    
    * feat(vta-890): replaced string literal
    
    * feat(vta-890): replaced string literal
    
    * feat(vta-890): removed dupe logger
    
    * feat(vta-890): when trl then use trailer id
    
    * feat(vta-890): changed the handler to return result not error
    
    * feat(vta-890): changed the handler catch block to log error
    
    * feat(vta-890): changed the handler catch block to log error
    
    * feat(vta-890): error logging
    
    * feat(vta-890): error logging
    
    * feat(vta-890): error logging
    
    * feat(vta-890): error logging
    
    * feat(vta-890): prettier
    
    * feat(vta-890): prettier
    
    * feat(vta-890): additional logger
    
    Co-authored-by: Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>
    
    Co-authored-by: Daniel Searle <84069850+Daniel-Searle@users.noreply.github.com>
    Co-authored-by: Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>
    
    * feat(vta-1123): added trailer id length restriction and test type filter by id
    
    * feat(vta-1123): corrected test type by id filter
    
    * feat(vta-1123): additional test type ids
    
    Co-authored-by: Melanie Coslett <50204104+m-coslett@users.noreply.github.com>
    Co-authored-by: Daniel Searle <Daniel.searle1@DVSA.GOV.UKDaniel.searle1@DVSA.GOV.UKdaniel.searle1@dvsa.gov.uk>

---
+++ src/utils/validators/McRequestSchema.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+import * as Joi from 'joi';
+
+export const mcRequestSchema = Joi.array().items({
+  vehicleIdentifier: Joi.string().required().max(8),
+  testDate: Joi.string().required(),
+  vin: Joi.string().required(),
+  testResult: Joi.string().required(),
+  hgvPsvTrailFlag: Joi.string().required(),
+  testResultId: Joi.string().required(),
+});
