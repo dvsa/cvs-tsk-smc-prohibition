8a0c5af49122aacaaf59694645db0865281cc234
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-05-04 17:12:21 +0100

    feat(vta-1331): added logger

--- src/handler.ts
+++ src/handler.ts
@@ -43,7 +43,7 @@ const handler = async (
       }
     }
   } else {
-    logger.info('Missing or not true environment variable present')
+    logger.info('Missing or not true environment variable present');
     callback(null, 'Missing or not true environment variable present');
   }
 };
