8ddd39d347fb7934d5a613cf21e8254f888e9872
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-05-04 15:59:19 +0100

    feat(vta-1331): kill switch

--- src/handler.ts
+++ src/handler.ts
@@ -6,10 +6,11 @@ import { sendMCProhibition } from './eventbridge/Send';
 import logger from './observability/Logger';
 import { MCRequest } from './utils/MCRequest';
 
-const { NODE_ENV, SERVICE, AWS_REGION, AWS_STAGE } = process.env;
+const { NODE_ENV, SERVICE, AWS_REGION, AWS_STAGE, SEND_TO_SMC } = process.env;
 
 logger.debug(
-  `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_STAGE}'\n region: '${AWS_REGION}'\n\n`,
+  `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_STAGE}'\n region: '${AWS_REGION}'\n 
+  Send to smc: '${SEND_TO_SMC}\n`,
 );
 
 const handler = async (
@@ -17,28 +18,32 @@ const handler = async (
   _context: Context,
   callback: Callback,
 ) => {
-  try {
-    logger.debug(`Function triggered with '${JSON.stringify(event)}'.`);
+  if (process.env.SEND_TO_SMC == 'true') {
+    try {
+      logger.debug(`Function triggered with '${JSON.stringify(event)}'.`);
 
-    // We want to process these in sequence to maintain order of database changes
-    for (const record of event.Records) {
-      const mcRequests: MCRequest[] = extractMCTestResults(record);
-      if (mcRequests != null) {
-        await sendMCProhibition(mcRequests);
+      // We want to process these in sequence to maintain order of database changes
+      for (const record of event.Records) {
+        const mcRequests: MCRequest[] = extractMCTestResults(record);
+        if (mcRequests != null) {
+          await sendMCProhibition(mcRequests);
+        }
+      }
+      callback(null, 'Data processed successfully.');
+    } catch (error) {
+      if (error.body) {
+        logger.error(JSON.stringify(error.body));
+        callback(
+          null,
+          `Data processed unsuccessfully: ${JSON.stringify(error.body)}`,
+        );
+      } else {
+        logger.error(error);
+        callback(null, `Data processed unsuccessfully: ${error}`);
       }
     }
-    callback(null, 'Data processed successfully.');
-  } catch (error) {
-    if (error.body) {
-      logger.error(JSON.stringify(error.body));
-      callback(
-        null,
-        `Data processed unsuccessfully: ${JSON.stringify(error.body)}`,
-      );
-    } else {
-      logger.error(error);
-      callback(null, `Data processed unsuccessfully: ${error}`);
-    }
+  } else {
+    logger.log(null, 'Incorrect environment variable present');
   }
 };
 
