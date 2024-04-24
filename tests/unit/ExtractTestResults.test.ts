b722a098820ebcf1f4ef097653b1d1386eb25727
Lulu Harvey <lulu.harvey@bjss.com>
2024-04-24 15:56:30 +0100

    chore(cb2-11283: fix tests

--- tests/unit/ExtractTestResults.test.ts
+++ tests/unit/ExtractTestResults.test.ts
@@ -2,15 +2,20 @@
 
 import { DynamoDBRecord } from 'aws-lambda';
 import { extractMCTestResults } from '../../src/utils/ExtractTestResults';
-import dynamoEventWCert from './data/dynamoEventWithCert.json';
+import { MCRequest } from '../../src/utils/MCRequest';
+import dynamoEventCancelled from './data/dynamoEventCancelled.json';
 import dynamoEventMultipleTests from './data/dynamoEventMultipleTestTypes.json';
 import dynamoEventMultipleTestTypesPassAndPrs from './data/dynamoEventMultipleTestTypesPassAndPrs.json';
-import dynamoEventCancelled from './data/dynamoEventCancelled.json';
-import { MCRequest } from '../../src/utils/MCRequest';
+import dynamoEventWCert from './data/dynamoEventWithCert.json';
+import dynamoEventWithoutVin from './data/dynamoEventWithoutVin.json';
 
 describe('extractTestResults', () => {
   let DYNAMO_DATA: DynamoDBRecord;
   let MC_RESULT: MCRequest[];
+
+  beforeEach(() => {
+    DYNAMO_DATA = null;
+  });
 
   it('when a test result has a status of cancelled then expect no mc requests are to be created', () => {
     DYNAMO_DATA = dynamoEventCancelled as DynamoDBRecord;
@@ -33,8 +38,7 @@ describe('extractTestResults', () => {
     expect(MC_RESULT).toHaveLength(1);
   });
   it('when processing a test result when there is a field missing a validation error is returned', () => {
-    DYNAMO_DATA = dynamoEventWCert as DynamoDBRecord;
-    DYNAMO_DATA.dynamodb.NewImage.vin = null;
+    DYNAMO_DATA = dynamoEventWithoutVin as DynamoDBRecord;
 
     try {
       extractMCTestResults(DYNAMO_DATA);
