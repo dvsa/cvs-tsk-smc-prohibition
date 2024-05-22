a5139f8f6de09e5728e5e69f66bd6f741cebf6b2
Ruairidh Taylor <ruairidh.taylor@bjss.com>
2024-05-22 15:15:39 +0100

    chore(cb2-12275): update packages

--- src/eventbridge/Send.ts
+++ src/eventbridge/Send.ts
@@ -1,3 +1,4 @@
+/* eslint-disable security/detect-object-injection */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
--- src/utils/validators/McRequestSchema.ts
+++ src/utils/validators/McRequestSchema.ts
@@ -1,12 +1,26 @@
 /* eslint-disable */
 
-import * as Joi from 'joi';
+import Joi from 'joi';
 
-export const mcRequestSchema = Joi.array().items({
-  vehicleIdentifier: Joi.string().required().max(8),
-  testDate: Joi.string().required(),
-  vin: Joi.string().required(),
-  testResult: Joi.string().required(),
-  hgvPsvTrailFlag: Joi.string().required(),
-  testResultId: Joi.string().required(),
-});
+export const mcRequestSchema = Joi.array().items(
+  Joi.object({
+    vehicleIdentifier: Joi.string().required().max(8).messages({
+      'any.required': '"vehicleIdentifier" is required',
+      'string.max': '"vehicleIdentifier" must be at most 8 characters long'
+    }),
+    testDate: Joi.string().required().messages({
+      'any.required': '"testDate" is required'
+    }),
+    vin: Joi.string().required().messages({
+      'any.required': '"vin" is required'
+    }),
+    testResult: Joi.string().required().messages({
+      'any.required': '"testResult" is required'
+    }),
+    hgvPsvTrailFlag: Joi.string().required().messages({
+      'any.required': '"hgvPsvTrailFlag" is required'
+    }),
+    testResultId: Joi.string().required().messages({
+      'any.required': '"testResultId" is required'
+    }),
+  }));
